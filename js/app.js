021bcc58f60198471f4ba330dcd41e42070dc146
William Rodrigues <williamrodrigues2442@gmail.com>
2021-12-01 22:30:04 -0300

    Muda localhost para API no Heroku

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const baseURL = 'http://localhost:3000';
+const baseURL = 'https://bs-api-rest.herokuapp.com';
 const api = new BookAPI(baseURL);
 
 const $booksWrapper = document.getElementById('books');
